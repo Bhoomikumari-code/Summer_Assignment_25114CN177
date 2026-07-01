2fd2d11acd0231d8994d32c9646e27f524f7bc9a
bhoomi <bhoomik282006@gmail.com>
2026-07-01 21:42:24 +0530

    Added day-3 program

--- day-3/Prime_number.c
+++ day-3/Prime_number.c
@@ -2,8 +2,14 @@
 #include<stdio.h>
 int main(){
     int n,i,flag=0;
+    
     printf("Enter the number:");
     scanf("%d",&n);
+    if(n<=1){
+        printf("Number is not prime");
+        return 0;
+    }
+    
     for(i=2;i<n;i++){
         if(n%i==0){
             flag=1;
